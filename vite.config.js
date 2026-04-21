737718fff6ccfa4553be9d02fe1cc7e0a3068158
George Amberson <George1984Amberson@yandex.ru>
2026-04-21 21:45:23 +0300

    add alias

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss()],
@@ -8,6 +9,11 @@ export default defineConfig({
   build: {
     outDir: '../wedding_prod',
     emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   server: {
     allowedHosts: true
